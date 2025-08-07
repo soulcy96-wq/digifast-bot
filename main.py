8c7470ee221890840c0c889faaba65f5f9928776
Souley Coulibaly <soulcy96@gmail.com>
2025-08-07 19:10:22 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+print("✅ DigiFast Bot lancé avec succès !")
+
+import time
+
+def run_bot():
+    while True:
+        print("⏳ Bot en cours d'exécution...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    run_bot()
